6350f0b52de84b927bf4c4d84570612063761c44
Abdulfatai Olarewaju <olarewaju Abdulfatai>
2025-11-24 13:20:52 +0100

    change in the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import BookContextProvider from "./hooks/bookContext";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-  <Provider store={store}>
+
+  <HashRouter>
+<Provider store={store}>
     <BookContextProvider>
    <App /> 
     </BookContextProvider>
   
   </Provider>
+  </HashRouter>
   
-  </BrowserRouter>
+  
+
 );
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
    darkMode : 'class',
+   base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
